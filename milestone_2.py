627b6595616d2428d2d52e391b6f66af1a52f59e
Nick <nicholas.gibbins@blueyonder.co.uk>
2022-11-02 23:08:50 +0000

    Adds user input with validation

--- milestone_2.py
+++ milestone_2.py
@@ -8,3 +8,14 @@ print(word_list)
 # Choose a random word from the list of possible words
 word = random.choice(word_list)
 print(word)
+
+# Get the user's guess using the input command
+guess = input("Please enter a letter: ")
+
+# Verify that the user's guess is a single character
+# By checking it is of length 1 and is alphabetical
+
+if len(guess)==1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input")
