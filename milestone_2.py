3b06c06beb495192fb1c11846c71cf0dac6fef1c
Nick <nicholas.gibbins@blueyonder.co.uk>
2022-11-02 17:16:41 +0000

    Imports random and chooses a random word

--- milestone_2.py
+++ milestone_2.py
@@ -1,3 +1,10 @@
 # My first AICore project - Hangman
+import random
+
+# Make a list to contain the possible words
 word_list = ["Apple", "Banana", "Mango", "Cherry", "Peach"]
 print(word_list)
+
+# Choose a random word from the list of possible words
+word = random.choice(word_list)
+print(word)
